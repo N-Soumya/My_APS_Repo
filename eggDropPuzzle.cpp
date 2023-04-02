57b10e00fb11b805e782ab704524a2a83b5b8029
N-Soumya <83869157+N-Soumya@users.noreply.github.com>
2023-04-02 19:38:53 +0530

    Create eggDropPuzzle.cpp

---
+++ eggDropPuzzle.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int minTrials(int n, int k)
+{
+	
+	vector<vector<int> > dp(k + 1, vector<int>(n + 1, 0));
+	int m = 0;
+	while (dp[m][n] < k) {
+		m++;
+		for (int x = 1; x <= n; x++) {
+			dp[m][x] = 1 + dp[m - 1][x - 1] + dp[m - 1][x];
+		}
+	}
+	return m;
+}
+
+
+int main()
+{
+	int n = 2, k = 36;
+	cout<<"Minimum number of trials "
+			"in worst case with "
+		<< n << " eggs and " << k << " floors is ";
+	cout << minTrials(2, 36);
+	return 0;
+}
+
+
