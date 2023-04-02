ea733432922e7a3fc04f114cfe16874aaa34183b
N-Soumya <83869157+N-Soumya@users.noreply.github.com>
2023-04-02 19:02:12 +0530

    Create diagmat.cpp

---
+++ diagmat.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter number of rows : ";
+    cin>>n;
+    int arr[n];
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n;j++)
+        {
+            cout<<"Enter element in [ "<<i<<","<<" "<<j<<" ] : ";
+            int x;
+            cin>>x;
+            if(i==j)
+            arr[i-1]=x;
+            cout<<endl;
+        }
+    }
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n;j++)
+        {
+            if(i==j)
+            cout<<arr[i-1]<<" ";
+            else
+            cout<<"0 ";
+        }
+        cout<<endl;
+    }
+
+}
